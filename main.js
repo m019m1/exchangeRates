da56aa3a76fd471039481ee90050bb6b99b52e49
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-09-22 22:52:10 +0700

    improving, optimizing, beautifying...

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
-const CURRENCIES = {
-	AUD:	'Australian dollar',
-	BGN:	'Bulgarian lev',
-	BRL:	'Brazilian real',
-	CAD:	'Canadian dollar',
-	CHF:	'Swiss franc',
-	CNY:	'Chinese yuan renminbi',
-	CZK:	'Czech koruna',
-	DKK:	'Danish krone',
-	EUR:	'European euro',
-	GBP:	'Pound sterling',
-	HKD:	'Hong Kong dollar',
-	HRK:	'Croatian kuna',
-	HUF:	'Hungarian forint',
-	IDR:	'Indonesian rupiah',
-	ILS:	'Israeli shekel',
-	INR:	'Indian rupee',
-	ISK:	'Icelandic krona',
-	JPY:	'Japanese yen',
-	KRW:	'South Korean won',
-	MXN:	'Mexican peso',
-	MYR:	'Malaysian ringgit',
-	NOK:	'Norwegian krone',
-	NZD:	'New Zealand dollar',
-	PHP:	'Philippine peso',
-	PLN:	'Polish zloty',
-	RON:	'Romanian leu',
-	RUB:	'Russian rouble',
-	SEK:	'Swedish krona',
-	SGD:	'Singapore dollar',
-	THB:	'Thai baht',
-	TRY:	'Turkish lira',
-	USD:	'US dollar',
-	ZAR:	'South African rand'
-};
+const CURRENCIES = [
+	['AUD', 'Australian dollar', 1],
+	['BGN',	'Bulgarian lev', 1],
+	['BRL',	'Brazilian real', 1],
+	['CAD',	'Canadian dollar', 1],
+	['CHF',	'Swiss franc', 1],
+	['CNY',	'Chinese yuan renminbi', 10],
+	['CZK',	'Czech koruna', 10],
+	['DKK',	'Danish krone', 10],
+	['EUR',	'European euro', 1],
+	['GBP',	'Pound sterling', 1],
+	['HKD',	'Hong Kong dollar', 10],
+	['HRK',	'Croatian kuna', 10],
+	['HUF',	'Hungarian forint', 100],
+	['IDR',	'Indonesian rupiah', 10000],
+	['ILS',	'Israeli shekel', 1],
+	['INR',	'Indian rupee', 100],
+	['ISK',	'Icelandic krona', 100],
+	['JPY',	'Japanese yen', 100],
+	['KRW',	'South Korean won', 1000],
+	['MXN',	'Mexican peso', 10],
+	['MYR',	'Malaysian ringgit', 1],
+	['NOK',	'Norwegian krone', 10],
+	['NZD',	'New Zealand dollar', 1],
+	['PHP',	'Philippine peso', 100],
+	['PLN',	'Polish zloty', 1],
+	['RON',	'Romanian leu', 1],
+	['RUB',	'Russian rouble', 100],
+	['SEK',	'Swedish krona', 10],
+	['SGD',	'Singapore dollar', 1],
+	['THB',	'Thai baht', 10],
+	['TRY',	'Turkish lira', 10],
+	['USD',	'US dollar', 1],
+	['ZAR',	'South African rand', 10]
+];
 
+let currencyFrom = document.getElementById('currencyFrom');
+let currencyTo = document.getElementById('currencyTo');
+let currencyPickerFrom = document.getElementById('currencyPickerFrom');
+let currencyPickerTo = document.getElementById('currencyPickerTo');
+
+let coinsFrom = document.getElementById('coinsFrom');
+let coinsTo = document.getElementById('coinsTo');
 initDate();
 initCurrencies();
+currencyPickerFrom.onchange = function() {showNominal(coinsFrom, event.currentTarget.value)};
+currencyPickerTo.onchange = function() {showNominal(coinsTo, event.currentTarget.value)};
+
+function showNominal(scoreboard, name) {
+	const index = ( CURRENCIES.map( currency => currency[0]) ).indexOf(name);
+	if(index == -1) return;
+	scoreboard.value = CURRENCIES[index][2];
+}
 
 function initDate() {
 	let today = new Date();
@@ -46,53 +61,68 @@ function initDate() {
 	day = day > 9 ? day : '0' + day;
 
 	let datePickers = document.getElementsByClassName('datePicker');
-	for(let datePicker of datePickers) {
-		datePicker.value = `${year}-${month}-${day}`;
-	}
+	datePickers[0].value = `${year}-${month}-01`;
+	datePickers[1].value = `${year}-${month}-${day}`;
 }
 
 function initCurrencies() {
 	let options = '';
-	for(let key in CURRENCIES) {
-		options += `<option value=${key}>${CURRENCIES[key]}</option>`;
+	for(let [name, description] of CURRENCIES) {
+		options += `<option value=${name}>${description}</option>`;
 	}
 	currencyFrom.innerHTML += options;
 	currencyTo.innerHTML += options;
 }
+
 let ctx = document.getElementById('myChart').getContext('2d');
-let myChart = new Chart(ctx, {
+let myChart = new Chart( ctx, {
  	type: 'line',
 	data: {
-			labels: [],
-			datasets: [{
-				
+			datasets: [
+				/* {
+					label: "please, enter the data above and press 'Convert' to retrieve exchange rates",
 					data: [],
-				
-			}]
+					backgroundColor: 'transparent',
+					borderColor: 'rgb(0, 186, 6)'
+				} */
+			]
 	},
 	options: {
-		scales: {
-				yAxes: [{
-						ticks: {
-								beginAtZero: true
-						}
-				}]
+		title: {
+			display: true,
+			text: `Exchange rate's chart`,
+			fontSize: 20,
+			fontColor: '#000',
+			lineHeight: 1
+		},
+		legend: {
+			labels: {
+				boxWidth: 15
+			}
+		},
+		tooltips: {
+			intersect: false,
+			callbacks: {
+				label: tooltipItem => (+tooltipItem.value).toFixed(2),
+				
+			},
+
 		}
 	}
-});;
+});
 
 function convert() {
-	const currencyFrom = document.getElementById('currency1').value;
-	const currencyTo = document.getElementById('currency2').value;
+	const curNameFrom = document.getElementById('currencyPickerFrom').value;
+	const curNameTo = document.getElementById('currencyPickerTo').value;
 	const dateFrom = document.getElementById('datePicker1').value;
 	const dateTo = document.getElementById('datePicker2').value;
-	if(!currencyFrom || !currencyTo) return false;
+
+	if(!curNameFrom || !curNameTo) return false;
 	
 	(async () => {
-		const url = `https://api.exchangeratesapi.io/history?start_at=${dateFrom}&end_at=${dateTo}&symbols=${currencyTo}&base=${currencyFrom}`;
+		const url = `https://api.exchangeratesapi.io/history?start_at=${dateFrom}&end_at=${dateTo}&symbols=${curNameTo}&base=${curNameFrom}`;
 		const response = await fetch(url);
 		const result = await response.json();
-		// console.log(result);
 		const sorted = Object.entries(result.rates).sort( ([key1, value1], [key2, value2]) => {
 			const a2 = Date.parse(key1);
 			const b2 = Date.parse(key2);
@@ -100,61 +130,27 @@ function convert() {
 		});
 		const rates = sorted.rates;
 		/* const dates = Object.keys(result.rates);
-		const values = Object.values(result.rates).map( values => values[`${currencyTo}`].toFixed(4)); */
+		const values = Object.values(result.rates).map( values => values[`${curNameTo}`].toFixed(4)); */
 		
 		//.values and .keys methods return random arrays! =((
 		let dates = [], values = [];
 		for(let [date, value] of sorted) {
 			dates.push(date);
-			values.push(value[currencyTo]);
+			values.push(value[curNameTo] * coinsFrom.value / coinsTo.value);
 		}
 
 		myChart.data.labels = dates;
-		myChart.data.datasets[0].data = values;
-		myChart.update();
+		myChart.data.datasets[0] = {
+			label: `${coinsFrom.value} ${curNameFrom} to ${coinsTo.value} ${curNameTo} exchange rate`,
+			data: values,
+			backgroundColor: 'transparent',
+			borderColor: 'rgb(0, 186, 6)'
+		};
 
+		myChart.update({
+			duration: 2000
+		});
 
-
-	/* 	myChart = new Chart(ctx, {
-			type: 'line',
-			data: {
-					// labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-					labels: dates,
-					datasets: [{
-							// label: '# of Votes',
-							// data: [12, 19, 3, 5, 2, 3],
-							data: values,
-						  backgroundColor: [
-									'rgba(255, 99, 132, 0.2)',
-									'rgba(54, 162, 235, 0.2)',
-									'rgba(255, 206, 86, 0.2)',
-									'rgba(75, 192, 192, 0.2)',
-									'rgba(153, 102, 255, 0.2)',
-									'rgba(255, 159, 64, 0.2)'
-							],
-							borderColor: [
-									'rgba(255, 99, 132, 1)',
-									'rgba(54, 162, 235, 1)',
-									'rgba(255, 206, 86, 1)',
-									'rgba(75, 192, 192, 1)',
-									'rgba(153, 102, 255, 1)',
-									'rgba(255, 159, 64, 1)'
-							], 
-							// borderWidth: 1
-					}]
-			},
-			options: {
-					scales: {
-							yAxes: [{
-									ticks: {
-											beginAtZero: true
-									}
-							}]
-					},
-					// responsive: true
-					// maintainAspectRatio: false
-			}
-		}); */
 		// const rate = result.rates[`${currencyTo}`].toFixed(4);
 		// const scoreboard = document.getElementById('result');
 		// scoreboard.innerHTML = `Current rate is ${rate}`;
